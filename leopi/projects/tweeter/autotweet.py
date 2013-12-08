7505edf8573a5a30e3f57180e21945a65d91ebdb
Erik Frister <erik.frister@aijko.com>
2013-12-08 14:50:07 +0100

    Add autotweet script

---
+++ leopi/projects/tweeter/autotweet.py
@@ -0,0 +1,34 @@
+from twython import *
+import configparser
+import sys
+
+# Read configuration
+configuration = {}
+try:
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    # Read all options into a dictionary
+    for section in config.sections():
+	configuration[section] = {}
+	for option in config.options(section):
+	    configuration[section][option] = config.get(section, option)
+
+except configparser.MissingSectionHeaderError as e:
+    print("Could not read configuration. Exiting")
+    exit()
+
+# Instantiate Twitter API with credentials
+oauth = configuration['OAuth']
+twitter = Twython(oauth['app_key'], oauth['app_secret'], oauth['oauth_token'], oauth['oauth_token_secret'])
+
+if 2 == len(sys.argv):
+    tweet = sys.argv[1]
+    try:
+	twitter.update_status(status=tweet)
+    except TwythonError as Error:
+	print("Could not authenticate with the Twitter API. Your API is probably set to Read-Only.")
+	print(format(Error))
+else:
+    print("Did not receive a parameter. Exiting.")
+    exit()
