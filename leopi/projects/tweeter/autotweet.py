d9cc83497c196d7bcf28161317647c0e83e7524f
Erik Frister <erik.frister@aijko.com>
2013-12-08 14:51:23 +0100

    Fix tabs

--- leopi/projects/tweeter/autotweet.py
+++ leopi/projects/tweeter/autotweet.py
@@ -10,9 +10,9 @@ try:
 
     # Read all options into a dictionary
     for section in config.sections():
-	configuration[section] = {}
-	for option in config.options(section):
-	    configuration[section][option] = config.get(section, option)
+        configuration[section] = {}
+        for option in config.options(section):
+            configuration[section][option] = config.get(section, option)
 
 except configparser.MissingSectionHeaderError as e:
     print("Could not read configuration. Exiting")
@@ -25,10 +25,10 @@ twitter = Twython(oauth['app_key'], oauth['app_secret'], oauth['oauth_token'], o
 if 2 == len(sys.argv):
     tweet = sys.argv[1]
     try:
-	twitter.update_status(status=tweet)
+        twitter.update_status(status=tweet)
     except TwythonError as Error:
-	print("Could not authenticate with the Twitter API. Your API is probably set to Read-Only.")
-	print(format(Error))
+        print("Could not authenticate with the Twitter API. Your API is probably set to Read-Only.")
+        print(format(Error))
 else:
     print("Did not receive a parameter. Exiting.")
     exit()
