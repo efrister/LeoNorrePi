6ed2f83e5fe6032c2d0077ee43c1df7b76b3360a
Erik Frister <erik.frister@aijko.com>
2014-02-22 16:09:27 +0100

    Try detecting rising and falling edge

--- leopi/projects/pir/pir.py
+++ leopi/projects/pir/pir.py
@@ -4,12 +4,28 @@ from time import strftime
 
 GPIO.setmode(GPIO.BOARD)
 PIR_PIN = 7
+EVENT_FALLING = "FALLING"
+EVENT_RISING = "RISING"
 
 GPIO.setup(PIR_PIN, GPIO.IN)
 
+last_event = ""
+
 def MOTION(PIR_PIN):
-    currentTime = strftime("%d.%m.%Y %H:%M:%S")
-    print("Motion Detected at ", currentTime)
+    current_time = strftime("%d.%m.%Y %H:%M:%S")
+    current_event = ""
+    if last_event == "" or last_event == EVENT_FALLING:
+        current_event = EVENT_RISING
+    else:
+        current_event = EVENT_FALLING
+
+    if current_event == EVENT_RISING:
+        msg = "Motion detected"
+    else:
+        msg = "PIR stopped sending last motion. Ready for next event"
+
+    last_event = current_event
+    print(msg, "at", current_time)
 
 print("PIR Module Test (CTRL+C to exit)")
 time.sleep(2)
